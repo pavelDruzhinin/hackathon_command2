100b1c7036cb283a5b02252378c46721e5ffd836
lenovo\van <vanfreeride@gmail.com>
2016-12-04 23:38:48 +0300

    Games pagination + games View exsample

--- GameShop/GameShop/App_Start/RouteConfig.cs
+++ GameShop/GameShop/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GameShop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Games", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
